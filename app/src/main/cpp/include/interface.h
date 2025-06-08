dda1fdff0cc9abffbafad55ad9db2faf9d7fe928
edy <bumingyang@ifengyu.com>
2025-06-08 10:46:19 +0800

    更换.a 文件和arm 架构引入配置头文件，可以编译成功但是库貌似引入不成功

--- app/src/main/cpp/include/interface.h
+++ app/src/main/cpp/include/interface.h
@@ -17,7 +17,7 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 #endif
 #if !defined(uint64_t)
-typedef unsigned long long uint64_t;
+typedef unsigned long  uint64_t;
 #endif
 #else
 // C99及以上环境直接包含标准头文件
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,26 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <android/log.h>
+#include "interface.h"
 
 //调用 libadd.so 动态库中的方法
-extern "C" {    //注意在 C++ 中调用 C 语言方法 , 需要做兼容设置
-extern int add(int a, int b);
-}
+//extern "C" {    //注意在 C++ 中调用 C 语言方法 , 需要做兼容设置
+//extern int add(int a, int b);
+//}
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_ifengyu_talk_TalkManager_stringFromJNI(JNIEnv *env, jobject thiz) {
     //
     //调用动态库中的函数
-    int sum = add(1, 2);
+//    int sum = add(1, 2);
 
     // Logcat 打印计算结果
-    __android_log_print(ANDROID_LOG_INFO, "JNI_TAG", "Native Caculate (CMake Build) : %d + %d = %d", 1, 2, sum);
+    __android_log_print(ANDROID_LOG_INFO, "JNI_TAG", "Native Caculate (CMake Build) : %d + %d ",
+                        1, 2);
 
     //将加法运算转为字符串 , 字符串如果不够长就报错 ...
     char str[100] = "0";
     //字符串格式化
-    sprintf(str, "Native Caculate : Static Library (CMake Build) : %d + %d = %d", 1, 2, sum);
+    sprintf(str, "Native Caculate : Static Library (CMake Build) : %d + %d ", 1, 2);
 
     return env->NewStringUTF(str);
 }
